033e22cd6fe3fbdeff71cfcc69b59c68be7a1be3
Isaac Cook <16isaaccook@gmail.com>
2024-01-22 16:14:59 -0700

    Added start of the Quill

---
+++ js/quill/main.js
@@ -0,0 +1,46 @@
+/*
+=== SECTIONS ===
+Overview
+    A location for quick access to things like recently worked on items, players, etc.
+World Objects
+    Where the magic happens. For creating and viewing objects such as locations, NPCs, deities, etc.
+    The types of objects are as follows:
+        - Articles
+            - General purpose text pages
+            - Players Handbook
+        - Buildings
+        - Characters
+        - Countries
+        - Deities
+        - Documents
+        - Ethnicities
+        - Geographic Locations
+        - Items
+        - Laws of Nature/Magic
+        - Languages
+        - Materials/Resources
+        - Military Conflicts
+        - Military Formations
+        - Myths & Legends
+        - Organizations
+        - Professions
+        - Religions
+        - Settlements
+        - Species
+        - Spells
+        - Technologies
+        - Traditions
+        - Vehicles
+Maps
+    Where maps are created, including things like image upload and POI tick creator
+History
+    A location for creating timelines, calendars, etc.
+Campaigns
+    A location for creating and viewing campaigns
+    Should have a section for allowing players to view certain world objects at the GM's approval
+    Has a World Handout section for creating a general overview of the setting for your players
+Writing Tools
+    A section for creating things like brainstorming pages, to-do lists, etc.
+Files
+    Contains quick access to things like images, as well as the actual markdown files themselves
+*/
