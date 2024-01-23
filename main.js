31f83e6f727703281a74b3bfb9b1e3c096214262
Isaac Cook <16isaaccook@gmail.com>
2024-01-22 21:52:12 -0700

    Added electron-reloader and electron-window-state packages with code to enable them in main.js

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
 const {app, BrowserWindow} = require('electron');
 const url = require('url');
 const path = require('path');
+const windowStateKeeper = require('electron-window-state');
+try {
+	require('electron-reloader')(module);
+} catch {}
  
+let devMode = true;
+
 let win;
  
 function createWindow() {
+    let mainWindowState = windowStateKeeper({
+        defaultWidth: 1366,
+        defaultHeight: 768,
+        maximize: false,
+        fullscreen: false
+    })
+
     win = new BrowserWindow({
         minWidth: 1366, 
         minHeight: 768,
-        autoHideMenuBar: true
+        x: mainWindowState.x,
+        y: mainWindowState.y,
+        width: mainWindowState.width,
+        height: mainWindowState.height
     });
+
     win.loadURL(url.format ({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
-    win.maximize();
+
+    mainWindowState.manage(win);
+
+    if(mainWindowState.isMaximized) {
+        win.maximize();
+    }
+    if(mainWindowState.isFullScreen) {
+        win.setFullScreen();
+    }
+    if(devMode) {
+        win.webContents.openDevTools();
+    }
 }
- 
+
 app.on('ready', createWindow);
